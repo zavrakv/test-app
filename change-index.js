dfb517fc8a45468d9cebe29073e2d48a35b4c225
Valentine <valentin.zavrak@px.com>
2025-01-16 16:38:12 +0200

    index huindex

---
+++ change-index.js
@@ -0,0 +1,24 @@
+﻿const fs = require("fs");
+const path = require("path");
+const {parse} = require("node-html-parser");
+
+const baseIndexHtmlPath = path.join(process.cwd(), `src/index.html`);
+
+const baseIndexHtmlFile = fs.readFileSync(baseIndexHtmlPath, {
+  encoding: "utf-8",
+});
+const index = parse(baseIndexHtmlFile);
+// index.querySelector("title").setAttribute("href", Math.random().toString());
+// console.log(index)
+// console.log("---------")
+const h = index.querySelector("title");
+h.insertAdjacentHTML("afterend", `<meta name="${Math.random().toString()}">`);
+const resultHtml = index.toString();
+
+fs.writeFileSync(
+  path.join(process.cwd(), `src/index.html`),
+  resultHtml,
+  {
+    encoding: "utf-8",
+  }
+);
